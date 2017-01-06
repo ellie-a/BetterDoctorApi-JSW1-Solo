f87469dcc94dc71ffa145c8a80b6703e353465b0
ea <elliea915@gmail.com>
2017-01-06 09:30:11 -0800

    recieves input from user and makes array

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,5 +1,17 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function(){
-  $('#time').text(moment());
+    $('#findDoctors').submit(function() {
+      event.preventDefault();
+      var medicalIssue = $('#issue').val();
+      console.log(medicalIssue);
+      $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+      .then(function(result) {
+    console.log(result);
+    })
+    .fail(function(error){
+    console.log("fail");
+    });
+    })
 });
